9b1a04bd0afe708e2b943ba4ef84b2444e974c96
福田 拓哉 <fukuda_takuya@ad.3-ize.local>
2024-08-02 12:03:11 +0900

    update

--- src/components/ApiFetch2.js
+++ src/components/ApiFetch2.js
@@ -1,60 +1,65 @@
-import React,{useState} from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const ApiFetch2 = () => {
-    const [inputText, setText] = useState(""); //入力ボックスのState関数
-    const [responseData, setResponseData] = useState(null); //APIのレスポンスのState
-    const [error, setError] = useState(null); //APIエラーの時のState
+  const [inputText, setText] = useState(""); //入力ボックスのState関数
+  const [responseData, setResponseData] = useState(null); //APIのレスポンスのState
+  const [error, setError] = useState(null); //APIエラーの時のState
 
-    const onChangeText = (event) => setText(event.target.value); //入力された項目を受け取る処理。この処理はある意味固定
+  const onChangeText = (event) => setText(event.target.value); //入力された項目を受け取る処理。この処理はある意味固定
 
-    const apiUrl2 = 'http://localhost:8000/manual/html/';
-    
-    const sendData = async () => {
-        try {
-            const newText = inputText; //入力された項目の受け取りと変数格納。
-            const response = await fetch(apiUrl2, {
-                method: 'POST', // POSTメソッドを使用
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    revice_type: "html",
-                    hedline : "見出し",
-                    text: newText, //inputTextでもよかったかも
-                })
-            });
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
+  const apiUrl2 = "http://localhost:8000/manual/html/";
 
-            // レスポンスをJSON形式に変換
-            const data = await response.json();
-            setResponseData(data); //JSONの受け取り
-            setError(null); //エラーなしで更新
-            setText(""); //実行後に入力ボックスを空にする
+  const sendData = async () => {
+    try {
+      const newText = inputText; //入力された項目の受け取りと変数格納。
+      const response = await fetch(apiUrl2, {
+        method: "POST", // POSTメソッドを使用
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          revice_type: "html",
+          hedline: "見出し",
+          text: newText, //inputTextでもよかったかも
+        }),
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
 
-        } catch (error) {
-            setError(error.message);
-            setResponseData(null);
-        }
+      // レスポンスをJSON形式に変換
+      const data = await response.json();
+      setResponseData(data); //JSONの受け取り
+      setError(null); //エラーなしで更新
+      setText(""); //実行後に入力ボックスを空にする
+    } catch (error) {
+      setError(error.message);
+      setResponseData(null);
     }
+  };
 
-    return (
+  return (
+    <div>
+      <h1>RagApp2 マニュアルHTML化</h1>
+      <textarea
+        placeholder="テキストを入力"
+        value={inputText}
+        onChange={onChangeText}
+      ></textarea>
+      <br />
+      <button onClick={sendData}>マニュアル作成</button>
+      <br />
+      {error && <pre>Error: {error}</pre>}
+      {responseData && (
         <div>
-            <h1>RagApp2 マニュアルHTML化</h1>
-                <textarea placeholder="テキストを入力" value={inputText} onChange={onChangeText}></textarea><br/>
-                <button onClick={sendData}>API②</button><br/>
-                {error && <pre>Error: {error}</pre>}
-                {responseData && (
-                    <div>
-                    <pre>{JSON.stringify(responseData, null, 2)}</pre>
-                    </div>
-                )}
-                <br/>
-                <Link to={`/`}>ホームへ戻る</Link>     
+          <pre>{JSON.stringify(responseData, null, 2)}</pre>
         </div>
-    )
-}
+      )}
+      <br />
+      <Link to={`/`}>ホームへ戻る</Link>
+    </div>
+  );
+};
 
-export default ApiFetch2
+export default ApiFetch2;
--- src/components/ApiFetch4.js
+++ src/components/ApiFetch4.js
@@ -1,62 +1,68 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const ApiFetch4 = () => {
-    const [inputText, setText] = useState(""); //入力ボックスのState関数
-    const [responseData, setResponseData] = useState(null); //APIのレスポンスのState
-    const [error, setError] = useState(null); //APIエラーの時のState
+  const [inputText, setText] = useState(""); //入力ボックスのState関数
+  const [responseData, setResponseData] = useState(null); //APIのレスポンスのState
+  const [error, setError] = useState(null); //APIエラーの時のState
 
-    const onChangeText = (event) => setText(event.target.value); //入力された項目を受け取る処理。この処理はある意味固定
+  const onChangeText = (event) => setText(event.target.value); //入力された項目を受け取る処理。この処理はある意味固定
 
-    const apiUrl = 'http://localhost:8000/ragapp/RAG/';
-    
-    const sendData = async () => {
-        try {
-            const newText = inputText; //入力された項目の受け取りと変数格納。
-            const response = await fetch(apiUrl, {
-                method: 'POST', // POSTメソッドを使用
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    "category": "クルマの予約",  //見出し
-                    "question": newText  //HTMLマニュアルファイルではなく直書きされたもの
-                })
-            });
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
+  const apiUrl = "http://44.219.78.185:8000/ragapp/RAG/";
 
-            // レスポンスをJSON形式に変換
-            const data = await response.json();
-            setResponseData(data); //JSONの受け取り
-            setError(null); //エラーなしで更新
-            setText(""); //実行後に入力ボックスを空にする
+  const sendData = async () => {
+    try {
+      const newText = inputText; //入力された項目の受け取りと変数格納。
+      const response = await fetch(apiUrl, {
+        method: "POST", // POSTメソッドを使用
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          category: "test", //見出し
+          question: newText, //HTMLマニュアルファイルではなく直書きされたもの
+        }),
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
 
-        } catch (error) {
-            setError(error.message);
-            setResponseData(null);
-        }
+      // レスポンスをJSON形式に変換
+      const data = await response.json();
+      setResponseData(data); //JSONの受け取り
+      setError(null); //エラーなしで更新
+      setText(""); //実行後に入力ボックスを空にする
+    } catch (error) {
+      setError(error.message);
+      setResponseData(null);
     }
+  };
 
-    return (
-        <div>
-            <h1>チャット画面</h1>
-                <textarea placeholder="テキストを入力" value={inputText} onChange={onChangeText}></textarea><br/>
-                <select id="dropdown">
+  return (
+    <div>
+      <h1>チャット画面</h1>
+      <textarea
+        placeholder="テキストを入力"
+        value={inputText}
+        onChange={onChangeText}
+      ></textarea>
+      <br />
+      {/* <select id="dropdown">
                     <option value="option1">クルマの予約</option>
-                </select><br/>
-                <button onClick={sendData}>API②</button><br/>
-                {error && <pre>Error: {error}</pre>}
-                {responseData && (
-                    <div>
-                    <pre>{JSON.stringify(responseData, null, 2)}</pre>
-                    </div>
-                )}
-                <br/>
-                <Link to={`/`}>ホームへ戻る</Link> {/* 追加　Go To page1をクリックするとhttp://localhost:3000/に遷移する */}            
+                </select><br/> */}
+      <button onClick={sendData}>回答生成</button>
+      <br />
+      {error && <pre>Error: {error}</pre>}
+      {responseData && (
+        <div>
+          <pre>{JSON.stringify(responseData, null, 2)}</pre>
         </div>
-    )
-}
+      )}
+      <br />
+      <Link to={`/`}>ホームへ戻る</Link>{" "}
+      {/* 追加　Go To page1をクリックするとhttp://localhost:3000/に遷移する */}
+    </div>
+  );
+};
 
-export default ApiFetch4
+export default ApiFetch4;
