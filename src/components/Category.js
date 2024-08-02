bdd742ee5ede8cef0426a860eb2f60669c4c4b26
福田 拓哉 <fukuda_takuya@ad.3-ize.local>
2024-08-02 15:12:11 +0900

    category add

--- src/components/Category.js
+++ src/components/Category.js
@@ -2,11 +2,28 @@ import React, { useState, useEffect } from "react";
 
 const Category = ({ setSelectedCategory }) => {
   const [options, setOptions] = useState([]);
+  const [error, setError] = useState(null);
+
+  const apiUrl = "http://44.219.78.185:8000/ragapp/category/";
 
   useEffect(() => {
-    const fetchData = () => {
-      const arr = ["test1", "test2", "test3", "test4", "test5"];
-      setOptions(arr);
+    const fetchData = async () => {
+      try {
+        const response = await fetch(apiUrl, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        if (!response.ok) {
+          throw new Error("Network response was not ok " + response.statusText);
+        }
+        // レスポンスをJSON形式に変換
+        const data = await response.json();
+        setOptions(data.category);
+      } catch (error) {
+        setError(error.message);
+      }
     };
 
     fetchData();
@@ -18,13 +35,17 @@ const Category = ({ setSelectedCategory }) => {
 
   return (
     <div>
-      <select id="dropdown" onChange={handleSelectChange}>
-        {options.map((option, index) => (
-          <option key={index} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
+      {error ? (
+        <div>Error: {error}</div>
+      ) : (
+        <select id="dropdown" onChange={handleSelectChange}>
+          {options.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      )}
       <br />
     </div>
   );
