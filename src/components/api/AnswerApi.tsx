6c5caf94bea6d4f1c22fe438627128c403b8f267
福田 拓哉 <fukuda_takuya@ad.3-ize.local>
2024-09-05 22:20:59 +0900

    category restore

--- src/components/api/AnswerApi.tsx
+++ src/components/api/AnswerApi.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import Category from "./Category";
 import styled from "styled-components";
 
 type Res = {
@@ -13,14 +12,59 @@ type Res = {
 
 const AnswerApi = () => {
   const [inputText, setText] = useState<string | number>(""); //入力ボックスのState関数
+  const [selectedCategory, setSelectedCategory] = useState<string>(""); //選択されたカテゴリーのState
+  const [options, setOptions] = useState<Array<string | number>>([]); //プルダウンのリスト
   const [responseData, setResponseData] = useState<Res | null>(null); //APIのレスポンスのState
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false); //APIエラーの時のState
-  const [selectedCategory, setSelectedCategory] = useState<string>(""); //選択されたカテゴリーのState
 
+  //テキスト入力時
   const onChangeText = (event: React.ChangeEvent<HTMLTextAreaElement>) =>
     setText(event.target.value); //入力された項目を受け取る処理。この処理はある意味固定
 
+  //プルダウン選択時
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  //カテゴリ生成用
+  const categoryapiUrl: string = "http://localhost:8000/ragapp/category/";
+
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+    const fetchData = async () => {
+      try {
+        const response = await fetch(categoryapiUrl, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include", // クッキーを含めるために必要
+        });
+        if (!response.ok) {
+          throw new Error("Network response was not ok " + response.statusText);
+        }
+        // レスポンスをJSON形式に変換
+        const data = await response.json();
+
+        // `category`配列から`h3`の値だけを取り出して`options`にセット
+        const categoryOptions = Array.from(
+          new Set<string>(data.category.map((item: { h2: string }) => item.h2))
+        );
+
+        setOptions(categoryOptions);
+      } catch {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  //回答生成用
   const apiUrl: string = "http://localhost:8000/ragapp/RAG/";
 
   const sendData = async (): Promise<void> => {
@@ -60,20 +104,34 @@ const AnswerApi = () => {
 
   return (
     <div>
-      <h1>チャット画面</h1>
+      <h1 style={{ textAlign: "center" }}>チャット画面</h1>
+      <h2>カテゴリ</h2>
       <p>
         プルダウンでバグ発生中。一回他の物を選択してから、選択したいやつ選択
       </p>
+      {error ? (
+        <pre style={{ color: "red" }}>データの取得に失敗しました。</pre>
+      ) : loading ? (
+        <pre>Loading...</pre>
+      ) : (
+        <select id="dropdown" onChange={handleSelectChange}>
+          {options.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      )}
+      {/* <Category setSelectedCategory={setSelectedCategory} /> */}
+      <br />
+      <h2>質問</h2>
+      <p>質問内容を入力してください</p>
       <StyledTextarea
         placeholder="テキストを入力"
         value={inputText}
         onChange={onChangeText}
       ></StyledTextarea>
       <br />
-      {/* <select id="dropdown">
-                    <option value="option1">クルマの予約</option>
-                </select><br/> */}
-      <Category setSelectedCategory={setSelectedCategory} />
       <ButtonContainer>
         <SButton onClick={sendData}>回答生成</SButton>
       </ButtonContainer>
