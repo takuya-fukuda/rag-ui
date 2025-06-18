e4971878bf301041ad2a3b7b1ebf43104ed3a1f7
福田 拓哉 <fukuda_takuya@ad.3-ize.local>
2025-06-18 14:24:40 +0900

    CSS修正

--- src/components/api/AnswerApi.tsx
+++ src/components/api/AnswerApi.tsx
@@ -102,10 +102,12 @@ const AnswerApi = () => {
 export default AnswerApi;
 
 const AllContainer = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
   height: 100vh;
 `;
+//
 
 const ContentContainer = styled.div`
   flex: 1; /* 残りのスペースを占める */
